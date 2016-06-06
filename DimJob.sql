d5313912efaee6fc846bf05c26fcb2543c3a7c72
Dennis O'Brien <dobrien@clayton.com>
2016-06-06 09:51:48 -0600

    Add DimJob.sql

---
+++ DimJob.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].DimJob
+(
+	JobSkey  INT IDENTITY(1,1) NOT NULL constraint PK_DimJob PRIMARY KEY,	
+	JobNo_NK  VARCHAR(20),
+	BusinessUnit  VARCHAR(80),
+	JobName VARCHAR(50),
+);
+
+GO
+
+CREATE UNIQUE INDEX [IX_DimJob_JobNo_NK] ON [dbo].[DimJob] (JobNo_NK)
